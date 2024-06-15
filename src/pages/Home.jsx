cb51b9cb357ca5c4712c874c60a27521a2230ce6
soniagkalliath <soniageorge111@gmail.com>
2024-06-15 15:59:55 +0530

    add project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,113 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import landingImg from '../assets/admin.png'
+import ProjectCard from '../components/ProjectCard'
+import { Card } from 'react-bootstrap'
+
+const Home = () => {
+  return (
+    <>
+      <div style={{minHeight:'100vh'}} className="d-flex justify-content-center align-items-center rounded shadow w-100">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6">
+              <h1 style={{fontSize:'80px'}}> <i className="fa-brands fa-docker"></i> Project Fair </h1>
+              <p style={{textAlign:'justify'}}>
+              One Stop Destination for all Software Development Projects. Where User can add and manage their projects. As well as access all projects available in our website... What are you waiting for!!!
+              </p>
+             { 
+             sessionStorage.getItem("token")?
+              <Link to="/dashboard" className="btn btn-warning" >MANAGE YOUR PROJECTS</Link>
+             :<Link to="/login" className="btn btn-warning" >START TO EXPLORE</Link>
+             }
+            </div>
+            <div className="col-lg-6">
+              <img className='img-fluid' src={landingImg} alt="landing" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-5 text-center">
+        <h1 className="mb-5">Explore Our Projects</h1>
+        <marquee>
+          <div className="d-flex">
+            <div className="me-5">
+              <ProjectCard/>
+            </div>
+          </div>
+        </marquee>
+        <button className='btn btn-link mt-5'>CLICK HERE TO VIEW MORE PROJECTS...</button>
+      </div>
+
+      <div className="d-flex align-items-center mt-5 flex-column">
+        <h1>Our Testimonials</h1>
+        <div className='d-flex align-items-center justify-content-evenly mt-3 w-100'>
+          <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title className='d-flex align-items-center justify-content-center flex-column'>
+                <img width={'60px'} height={'60px'} className='rounded-circle img-fluid' src="https://www.pngplay.com/wp-content/uploads/12/User-Avatar-Profile-PNG-Free-File-Download.png" />
+                <span>Max Miller</span>
+                </Card.Title>
+                <Card.Text>
+                  <div className="d-flex justify-content-center">
+                    <div className="fa-solid fa-star text-warning"></div>
+                    <div className="fa-solid fa-star text-warning"></div>
+                    <div className="fa-solid fa-star text-warning"></div>
+                    <div className="fa-solid fa-star text-warning"></div>
+                  </div>
+                  <p>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </p>
+                </Card.Text>
+              </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title className='d-flex align-items-center justify-content-center flex-column'>
+                <img width={'60px'} height={'60px'} className='rounded-circle img-fluid' src="https://cdn3.iconfinder.com/data/icons/avatars-flat/33/woman_9-512.png" />
+                <span>Nina Halsen</span>
+                </Card.Title>
+                <Card.Text>
+                  <div className="d-flex justify-content-center">
+                    <div className="fa-solid fa-star text-warning"></div>
+                    <div className="fa-solid fa-star text-warning"></div>
+                    <div className="fa-solid fa-star text-warning"></div>
+                    <div className="fa-solid fa-star text-warning"></div>
+                    <div className="fa-solid fa-star text-warning"></div>
+                  </div>
+                  <p>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </p>
+                </Card.Text>
+              </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title className='d-flex align-items-center justify-content-center flex-column'>
+                <img width={'60px'} height={'60px'} className='rounded-circle img-fluid' src="https://cdn3.iconfinder.com/data/icons/avatars-flat/33/man_5-1024.png" />
+                <span>Liam Anderson</span>
+                </Card.Title>
+                <Card.Text>
+                  <div className="d-flex justify-content-center">
+                    <div className="fa-solid fa-star text-warning"></div>
+                    <div className="fa-solid fa-star text-warning"></div>
+                    <div className="fa-solid fa-star text-warning"></div>
+                    <div className="fa-solid fa-star text-warning"></div>
+                  </div>
+                  <p>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </p>
+                </Card.Text>
+              </Card.Body>
+          </Card>
+        </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default Home
